9a6884e576c4fcc9c2c8ceeb322b3befba797ac6
jc0803kevin <jc0803kevin@163.com>
2020-03-29 11:25:38 +0800

    spring Bean  实例化

--- spring-bean/src/main/java/com/kevin/thinking/in/spring/bean/definition/BeanInstantiationDemo.java
+++ spring-bean/src/main/java/com/kevin/thinking/in/spring/bean/definition/BeanInstantiationDemo.java
@@ -1,12 +1,5 @@
 package com.kevin.thinking.in.spring.bean.definition;
 
-import com.kevin.thinking.in.spring.bean.factory.DefaultUserFactory;
-import com.kevin.thinking.in.spring.bean.factory.UserFactory;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.context.annotation.Bean;
-
-import java.util.Arrays;
-
 /**
  * @Author kevin
  * @Description Bean 实例化
@@ -15,23 +8,7 @@ import java.util.Arrays;
 public class BeanInstantiationDemo {
 
     public static void main(String[] args) {
-        AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext();
-        // 注册 Configuration Class（配置类）
-        applicationContext.register(BeanInstantiationDemo.class);
-        // 启动 Spring 应用上下文
-        applicationContext.refresh();
 
-        // 依赖查找 UserFactory
-        UserFactory userFactory = applicationContext.getBean(UserFactory.class);
-        System.out.println(userFactory);
-
-        // 关闭 spring 应用上下文
-        applicationContext.close();
-    }
-
-    @Bean
-    public UserFactory userFactory(){
-        return new DefaultUserFactory();
     }
 
 }
